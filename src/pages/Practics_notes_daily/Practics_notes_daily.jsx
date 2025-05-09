7f2acff8b1e4d12a67d87ffad136346453add653
naveensainijpr@gmail.com <naveensainijpr@gmail.com>
2025-05-09 13:29:26 +0530

    fusj

--- src/pages/Practics_notes_daily/Practics_notes_daily.jsx
+++ src/pages/Practics_notes_daily/Practics_notes_daily.jsx
@@ -16,7 +16,7 @@ export default function Practics_notes_daily() {
       </div>
 
       {/* tabs start */}
-      <div className="flex space-x-4 bg-gray-100 p-3 rounded-lg shadow-md">
+      <div className="flex space-x-2 bg-gray-100 p-3 rounded-lg shadow-md">
         <button
           className={`md:px-4 px-2 py-2 text-lg md:text-xl font-bold rounded-md transition-all duration-300 ${
             activButton === "button1"
@@ -49,20 +49,30 @@ export default function Practics_notes_daily() {
         >
           Node.js
         </button>
+        <button
+          className={`md:px-4 px-2 py-2 text-lg md:text-xl font-bold rounded-md transition-all duration-300 ${
+            activButton === "button4"
+              ? "text-blue-600 border-b-4 border-black"
+              : "text-[#a52a2a] hover:text-gray-800"
+          }`}
+          onClick={() => setactivButton("button4")}
+        >
+          Wordpress
+        </button>
       </div>
 
       {/* tabs end */}
 
-      <div className="bg-slate-400 ">
+      <div className="bg-slate-400">
         {activButton === "button1" ? (
           <Javascript />
         ) : activButton === "button2" ? (
           <Reactnotes />
         ) : activButton === "button3" ? (
           <Node_js />
-        ) : (
-          ""
-        )}
+        ) : activButton === "button4" ? (
+          <Wordpress />
+        ) : null}
       </div>
     </>
   );
@@ -223,6 +233,62 @@ function Node_js() {
                   <p
                     className={`p-3 bg-[black] text-white font-medium transition-all duration-1000 ease-in-out 
       ${v._id === nodejsId ? "block opacity-100" : "hidden opacity-0"}
+    `}
+                  >
+                    <pre className="whitespace-pre-wrap break-words">
+                      {v.Answers}
+                    </pre>
+                  </p>
+                </div>
+              </div>
+            );
+          })
+        : "no data show"}
+    </div>
+  );
+}
+
+function Wordpress() {
+  const [wordpressId, setwordpressId] = useState(null); // currentId स्टेट बना रहे हैं, शुरुआत में यह null है
+  const [wordpressshow, setwordpressshow] = useState([]); // currentId स्टेट बना रहे हैं, शुरुआत में यह null है
+
+  useEffect(() => {
+    axios
+      .post("https://rss-feed-node-js.onrender.com/api/backend/WordPress/view")
+      .then((result) => {
+        setwordpressshow(result.data.data);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }, []);
+  const toggleFAQ = (id) => {
+    console.log("javascript", id);
+    setwordpressId(wordpressId === id ? null : id); // currentId को अपडेट कर रहे हैं, यदि वही FAQ फिर से क्लिक किया गया है तो इसे बंद करें
+  };
+  return (
+    <div>
+      <h2 className="text-3xl font-bold text-blue-600 text-center py-[20px]">
+        WordPress
+      </h2>
+      {wordpressshow.length > 0
+        ? wordpressshow.map((v, i) => {
+            return (
+              <div key={i}>
+                <div className="border rounded-xl p-4 shadow-md">
+                  <button
+                    className="w-full text-left font-semibold text-lg text-gray-800 focus:outline-none relative"
+                    onClick={() => toggleFAQ(v._id)}
+                  >
+                    <span className="mx-2">{i + 1}</span>
+                    {v.Question}
+                    <span className="absolute right-[10px] top-[10px]">
+                      {v._id === wordpressId ? <FaMinus /> : <FaPlus />}
+                    </span>
+                  </button>
+                  <p
+                    className={`p-3 bg-[black] text-white font-medium transition-all duration-1000 ease-in-out 
+      ${v._id === wordpressId ? "block opacity-100" : "hidden opacity-0"}
     `}
                   >
                     <pre className="whitespace-pre-wrap break-words">
