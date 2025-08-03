ca35a98898af11bdde6dade2b6fa77c24f4eb79c
naveen saini <naveensainijpr@gmail.com>
2025-08-03 12:25:26 +0530

    djeui

--- src/pages/toolkit-project/toolkit.jsx
+++ src/pages/toolkit-project/toolkit.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import Checkboxslice from './pages/Checkboxslice'
 import CounterSlice from './pages/CounterSlice'
+import Header from '../../Common page/Header'
 
 export default function Toolkit() {
   return (
     <>
+    <Header/>
     <Checkboxslice />
     <CounterSlice/>
     </>
